10963f9960ed88fa426f0640193c4b5f1d5acda7
Ameli98 <rain0210303@gmail.com>
2023-02-24 19:07:07 +0800

    init submit

--- auto_dict.py
+++ auto_dict.py
@@ -1,10 +1,50 @@
-class auto_dict():
-    def __init__(self, text_file:str, web_dict:str) -> None:
-        self.wordset = self.take_word(text_file)
+from pathlib import Path
+import csv
 
-    def take_word(self, file:str) -> list:
-        with open(file, "r") as f:
-            content = f.read()
+class auto_dict():
+    def __init__(self, text_file:Path, WebDict_File:Path="EN_DefaultDict.csv") -> None:
+        # text_file : txt file with target words which is split with "/n"
+        # WebDict_File : csv file with root path of the website dictionary which is split with "/n"
+        # e.g. For apple in Cambridge dictionary : https://dictionary.cambridge.org/dictionary/english-chinese-traditional/apple
+        # Use https://dictionary.cambridge.org/dictionary/english-chinese-traditional/ as root path in your file
+        self.wordset = self.get_word(text_file)
+        self.dictname, self.dictroot = self.get_dict(WebDict_File)
+        self.result_file = Path(f"{text_file.stem}.html")
+        self.write_result()
+
+    # Get words in the txt file
+    def get_word(self, text_file:Path) -> list:
+        with open(text_file, "r") as tf:
+            content = tf.read()
         return content.split("\n")
 
-    
+    # Get dictionaries in the csv file
+    def get_dict(self, csv_file:Path) -> tuple:
+        with open(csv_file, "r") as df:
+            reader = csv.DictReader(df)
+        return [row["dictname"] for row in reader], [row["dictroot"] for row in reader]
+
+
+    # Assemble result
+    def write_result(self) -> None:
+        result_file = Path()
+        with open(self.result_file, "w") as rf:
+            html_begin = ["<!DOCTYPE html>\n", "<html>\n", "  <head>\n", f"    <title>{self.result_file.stem}</title>\n", "  </head>\n", "  <body>\n"]
+            rf.writelines(html_begin)
+            for word in self.wordset:
+                rf.write(f"  <h1>{word}</h1>\n  <p>")
+                #TODO: write the hyperlink of dicts' result in the html.
+                for dictname, dictroot in zip(self.dictname, self.dictroot):
+                    rf.write(f"<a href={dictroot+word}>{dictname}</a> ")
+                rf.write("</p>\n")
+            html_end = ["  </body>\n", "</html>"]
+            rf.writelines(html_end)
+
+if __name__ == "__main__":
+    from sys import argv
+    try:
+        text_file, WebDict_File = Path(argv[1]), Path(argv[2])
+        Dictionary = auto_dict(text_file, WebDict_File)
+    except IndexError:
+        print("Except a target txt file and a dictionary csv file")
+        print("try: Python3 auto_dict.py <target txt> <dictionary csv>")
